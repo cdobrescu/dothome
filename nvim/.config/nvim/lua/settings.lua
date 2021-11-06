3a9a8d57271487014b4cd54f18b974c70146ddd0
Cristian Dobrescu <cristian.dobrescu@metaswitch.com>
2021-11-06 22:54:01 +0200

    Add indent settings

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -5,14 +5,20 @@ vim.opt.pumblend       = 10           -- pseudo-transparency for popup-menu
 vim.opt.termguicolors  = true         -- true color support
 vim.opt.relativenumber = true         -- show relative numbers in gutter
 vim.opt.scrolloff      = 3            -- start scrolling 3 lines before edge of viewport
-vim.opt.expandtab      = true         -- always use spaces instead of tabs
 
-vim.opt.shiftround     = true         -- Round indent ?
 vim.opt.smartcase      = true         -- Do not ignore case with captials
-vim.opt.smartindent    = true         -- Inser indents automatically
 
 vim.g.hidden           = true         -- allow buffer switching without prior saving
 vim.g.confirm          = true         -- demand confirmation when closing unsaved buffers
+
+
+------------------- INDENTATION SETTINGS ---------------------------------------------------
+vim.opt.autoindent     = true         -- Round indent ?
+vim.opt.cindent        = true         -- Strincter indenting rules for C programs
+vim.opt.shiftround     = true         -- Round indent ?
+vim.opt.shiftwidth     = 2            -- Set indenting to 2 spaces
+vim.opt.smartindent    = true         -- Inser indents automatically
+vim.opt.expandtab      = true         -- always use spaces instead of tabs
 
 vim.cmd 'colorscheme gruvbox'
 vim.g.background       = 'dark'
